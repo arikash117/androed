59dd02b99d7a48dee24804417c2b4f6411c76655
Arina <ari.kash117@gmail.com>
2025-10-07 00:00:54 +0300

    Верстка экрана авторизации/регистрации

--- app/src/main/java/com/example/ariandroid/ui/screens/authorization/AuthorizationScreen.kt
+++ app/src/main/java/com/example/ariandroid/ui/screens/authorization/AuthorizationScreen.kt
@@ -1,2 +1,131 @@
 package com.example.ariandroid.ui.screens.authorization
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ariandroid.R
+import com.example.ariandroid.ui.theme.Background
+import com.example.ariandroid.ui.theme.BlackCurrant
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun AuthorizationScreen(
+    onSplashEnd: () -> Unit = {}
+) {
+    LaunchedEffect(Unit) {
+        delay(2500)
+        onSplashEnd()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 20.dp)
+                .systemBarsPadding()
+        ) {
+            //Заголовок
+            Text(
+                text = "DriveNext",
+                color = BlackCurrant,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Поможем найти твою следующую поездку",
+                color = Color(0xB3000000),
+                fontSize = 16.sp,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth(),
+                lineHeight = 20.sp
+            )
+
+            Spacer(modifier = Modifier.height(110.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.icon),
+                contentDescription = "DriveNext Logo",
+                modifier = Modifier.size(350.dp)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            //Колонна кнопок
+            Column () {
+                // Войти
+                TextButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 350.dp, height = 50.dp)
+                        .background(
+                            color = BlackCurrant, shape = RoundedCornerShape(8.dp)
+                        ),
+                ) {
+                    Text(
+                        text = "Войти",
+                        color = Color(0xFFFFFFFF),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                // Зарегистрироваться
+                TextButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 350.dp, height = 50.dp)
+                        .background(
+                            color = Background, shape = RoundedCornerShape(8.dp)
+                        )
+                        .border(1.dp, Color.Gray, shape = RoundedCornerShape(8.dp))
+                ) {
+                    Text(
+                        text = "Зарегистрироваться",
+                        color = Color(0xFF344054),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+        }
+    }
+}
