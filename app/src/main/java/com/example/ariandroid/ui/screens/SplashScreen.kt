08f4f768643db0fda40f6ed462b27f9f957b17f5
Arina <ari.kash117@gmail.com>
2025-10-06 21:50:59 +0300

    Верстка SplashScreen.kt

---
+++ app/src/main/java/com/example/ariandroid/ui/screens/SplashScreen.kt
@@ -0,0 +1,86 @@
+package com.example.ariandroid.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ariandroid.R
+import com.example.ariandroid.ui.theme.Background
+import com.example.ariandroid.ui.theme.BlackCurrant
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun SplashScreen(
+    onSplashEnd: () -> Unit = {}
+) {
+    LaunchedEffect(Unit) {
+        delay(2500)
+        onSplashEnd()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 20.dp)
+                .padding(bottom = 216.dp, top = 80.dp)
+                .systemBarsPadding()
+        ) {
+            //Заголовок
+            Text(
+                text = "DriveNext",
+                color = BlackCurrant,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Поможем найти твою следующую поездку",
+                color = Color(0xB3000000),
+                fontSize = 16.sp,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.fillMaxWidth(),
+                lineHeight = 20.sp
+            )
+
+            Spacer(modifier = Modifier.height(110.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.icon),
+                contentDescription = "DriveNext Logo",
+                modifier = Modifier.size(350.dp)
+            )
+        }
+    }
+}
