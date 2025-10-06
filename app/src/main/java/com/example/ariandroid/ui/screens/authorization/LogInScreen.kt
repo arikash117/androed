b9c00bc35340cecc07312f9dadd90a497c064960
Arina <ari.kash117@gmail.com>
2025-10-07 01:19:51 +0300

    Добавление на экран архитектуры объектов (верстка)

--- app/src/main/java/com/example/ariandroid/ui/screens/authorization/LogInScreen.kt
+++ app/src/main/java/com/example/ariandroid/ui/screens/authorization/LogInScreen.kt
@@ -1,2 +1,165 @@
 package com.example.ariandroid.ui.screens.authorization
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ariandroid.R
+import com.example.ariandroid.ui.theme.Background
+import com.example.ariandroid.ui.theme.BlackCurrant
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun LogInScreen(
+    onSplashEnd: () -> Unit = {}
+) {
+    LaunchedEffect(Unit) {
+        delay(2500)
+        onSplashEnd()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp)
+                .systemBarsPadding()
+        ) {
+            //Ввойдите в аккаунт
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Ввойдите в аккаунт",
+                    color = Color(0xFF1A1A1A),
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center,
+                    fontSize = 24.sp,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Пожалуйста, введите свои данные",
+                    color = Color.Gray,
+                    fontSize = 14.sp,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(65.dp))
+
+            // Форма + кнопки
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Форма
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+
+                }
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                //кнопки
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    // Войти
+                    TextButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .size(width = 350.dp, height = 50.dp)
+                            .background(
+                                color = BlackCurrant, shape = RoundedCornerShape(8.dp)
+                            ),
+                    ) {
+                        Text(
+                            text = "Войти",
+                            color = Color(0xFFFFFFFF),
+                            style = MaterialTheme.typography.bodyLarge,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    // Войти через Google
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .size(width = 350.dp, height = 50.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color(0xFF1A1A1A),
+                        ),
+                        border = BorderStroke(1.dp, Color.Gray),
+                        shape = RoundedCornerShape(8.dp),
+                    ) {
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Image(
+                                painterResource(id = R.drawable.google_icon),
+                                contentDescription = "Google icon",
+                                modifier = Modifier.size(20.dp)
+                            )
+
+                            Spacer(modifier = Modifier.width(12.dp))
+
+                            Text(
+                                text = "Войти через Google",
+                                color = Color(0xFF1A1A1A),
+                                style = MaterialTheme.typography.bodyLarge,
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(65.dp))
+
+            // Зарегистрироваться
+            Text(
+                text = "Зарегистироваться",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
