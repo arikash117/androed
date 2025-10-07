520d9876fe53c1b577d417b1d0d126285d4ea986
Arina <ari.kash117@gmail.com>
2025-10-07 17:06:10 +0300

    Верстка последнего экрана регистрации (окончание регистрации)

---
+++ app/src/main/java/com/example/ariandroid/ui/screens/authorization/Congratulations.kt
@@ -0,0 +1,117 @@
+package com.example.ariandroid.ui.screens.authorization
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ariandroid.R
+import com.example.ariandroid.ui.theme.Background
+import com.example.ariandroid.ui.theme.BlackCurrant
+
+@Preview
+@Composable
+fun Congratulations(
+
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp).padding(top = 16.dp, bottom = 32.dp)
+                .systemBarsPadding()
+        ) {
+
+            Text (
+                text = "Пользуйтесь с удовольствием",
+                style = MaterialTheme.typography.bodyLarge,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 36.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.success),
+                    contentDescription = "Success photo",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // TEXTs
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Поздравляем",
+
+                    )
+                    Text(
+                        text = "Вы успешно зарегистрировались. Обработка ваших персональных данных займет около двух часов. Спасибо за ожидание.",
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            TextButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(width = 350.dp, height = 50.dp)
+                    .background(
+                        color = BlackCurrant, shape = RoundedCornerShape(8.dp)
+                    ),
+            ) {
+                Text(
+                    text = "Далее",
+                    color = Color(0xFFFFFFFF),
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
+        }
+    }
+}
