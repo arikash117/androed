aac84199f025933aeef2a0f8a15713ea11b28d25
Arina <ari.kash117@gmail.com>
2025-10-30 20:07:55 +0300

    Верстка экрана загрузки

---
+++ app/src/main/java/com/example/ariandroid/presentation/screens/home/LoadingScreen.kt
@@ -0,0 +1,60 @@
+package com.example.ariandroid.presentation.screens.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ariandroid.R
+import com.example.ariandroid.ui.theme.Background
+import com.example.ariandroid.ui.theme.BlackCurrant
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun SplashScreen(
+    onSplashEnd: () -> Unit = {}
+) {
+    LaunchedEffect(Unit) {
+        delay(2500)
+        onSplashEnd()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.load_car),
+                contentDescription = "searching cars",
+                modifier = Modifier.size(96.dp)
+            )
+            Text(text = "Ищем подходящие автомобили")
+            Spacer(modifier = Modifier.height(8.dp))
+            CircularProgressIndicator(
+                color = BlackCurrant,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+}
